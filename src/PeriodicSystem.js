45c8617703be32a235e38a588cd92b7e07b866e8
Rosanna Lomartire <ebblom1014@staden.gotheborg.net>
2022-02-26 20:29:17 +0100

    UI wip

--- src/PeriodicSystem.js
+++ src/PeriodicSystem.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+
 import './App.css';
-// import getData from './ApiModule'
-// import axios from "axios";
 
 const axios = require("axios").default;
 
@@ -19,6 +22,7 @@ const PeriodicSystem = () => {
 
   const [data, setData] = useState([])
 
+
   const fetchApiData = () => {
     let apiData = [{}]
     axios.request(options).then(function (response) {
@@ -30,30 +34,38 @@ const PeriodicSystem = () => {
   });
     return apiData
   }
-  if (data) {
-    console.log('data:', data)
-    data.map((item) => {
-      console.log('item: ', item)
-      return {
-        period: item.period,
-        name: item.name,
-        atomicmass: item.atomicmass,
-        group: item.group,
-        symbol: item.symbol,
-      }
-    })
-  }
 
-  // fetchApiData()
+
+
+console.log('data: ', data)
+
+
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <button onClick={fetchApiData}>Ladda ner systemet</button>
-        <p>Hej</p>
-      </header>
-    </div>
-  );
-}
+    <Box className="App">
+      <h1>Periodic Table</h1>
+      <Button onClick={fetchApiData} color="primary" variant="contained">Ladda ner data för systemet</Button>
+        <Box display="flex" justifyContent="row" flexWrap="wrap" marginTop="20px">
+          
+          {data.map((item, idx) => {
+          return  <Box key={`${item}-${idx}`} bgcolor={"#" + item.cpkHexColor} width="200px" margin="5px">
+           <Typography align="left">{item.atomicNumber}</Typography>
+           <Typography variant="h2">{item.symbol}</Typography> 
+           <Typography align="left">{item.name}</Typography>
+           <Typography align="left">{item.atomicMass}</Typography> 
+            </Box>
+          })}
+
+           
+          </Box>
+      </Box>
+ )}
+
 
 export default PeriodicSystem;
+        // <button onClick={fetchApiData}>Ladda ner data för systemet</button>
+        // <p>Perodic Table</p>
+      // </header>
+    //</div>
+ // );
+//}
